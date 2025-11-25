d4ecf2aa6620610fe5d0d4fc0b3c566e7cb679e0
oiiemon9 <oiiemon9@gmail.com>
2025-11-25 13:43:46 +0600

    edit

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,7 +4,7 @@ import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
-export default function loginPage() {
+export default function LoginPage() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/';
   return (
